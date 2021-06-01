318633b05c694f8e0ae627c53150229a815c5b5e
YourWebsite-Code <78820495+YourWebsite-Code@users.noreply.github.com>
2021-06-01 13:36:16 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
 function preload() {
     song = loadSound("Harry Potter - Main Theme.mp3")
@@ -24,6 +26,10 @@ function setup() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX =" + leftWristX + " leftWristY = " + leftWristY);
@@ -40,10 +46,33 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
-}
+    song1status = song.isPlaying()
+    song2status = song1.isPlaying()
 
-function play() {
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
-}
+    fill("FF0000");
+    stroke("FF0000");
+
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song.stop();
+        if (song2status == false) {
+            song1.play()
+            document.getElementById("song").innerHTML = "Playing = Harry Potter - Metal Version.mp3";
+        }
+
+    }
+
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if (song2status == false) {
+            song.play()
+            document.getElementById("song").innerHTML = "Playing = Harry Potter - Main Theme.mp3";
+        }
+    }
+
+    function play() {
+        song.play();
+        song.setVolume(1);
+        song.rate(1);
+    }
